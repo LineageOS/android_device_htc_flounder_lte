LineageOS/android_device_htc_flounder_lte

commit b4800a97ac25dd0f2bcf2e770002c38c8356da5e

flounder_lte: update build fingerprint & description to N4F26Q
Change-Id: I7839b03f2eff926911ed5e657e0b233cf26af94d

lineage.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -51,8 +52,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder_lte \
-    BUILD_FINGERPRINT=google/volantisg/flounder_lte:7.1.1/NMF26F/3425388:user/release-keys \
-    PRIVATE_BUILD_DESC="volantisg-user 7.1.1 NMF26F 3425388 release-keys"
+    BUILD_FINGERPRINT=google/volantisg/flounder_lte:7.1.1/NMF26Q/3623443:user/release-keys \
+    PRIVATE_BUILD_DESC="volantisg-user 7.1.1 NMF26Q 3623443 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_flounder_lte

commit 9bf06eace22a978bed4469e6bbf76e472745e997

flounder_lte: really update fingerprint to N4F26Q
Fixes: b4800a97ac25dd0f2bcf2e770002c38c8356da5e
Change-Id: I9bb3193347511a6c08b1b42d14d89e7aba1826b5

lineage.mk
@@ -52,8 +52,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder_lte \
-    BUILD_FINGERPRINT=google/volantisg/flounder_lte:7.1.1/NMF26Q/3623443:user/release-keys \
-    PRIVATE_BUILD_DESC="volantisg-user 7.1.1 NMF26Q 3623443 release-keys"
+    BUILD_FINGERPRINT=google/volantisg/flounder_lte:7.1.1/N4F26Q/3623443:user/release-keys \
+    PRIVATE_BUILD_DESC="volantisg-user 7.1.1 N4F26Q 3623443 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_flounder_lte